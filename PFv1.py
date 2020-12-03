4cc85f652107b99dea5fdf93d842fa57a45a6efe
viveliduc <viveliduc2@gmail.com>
2020-12-03 15:40:52 -0600

    Validation of results
    Validating with flags if there is a nose or mouth and tagging if it is a good citizen or a covidiot.

--- PFv1.py
+++ PFv1.py
@@ -11,33 +11,61 @@ if mouth_cascade.empty():
   
 if nose_cascade.empty():
   raise IOError('Unable to load the nose cascade classifier xml file')
+
+if face_cascade.empty():
+  raise IOError('Unable to load the face cascade classifier xml file')
   
 cap = cv2.VideoCapture(0)
 ds_factor = 0.5
+flagFace=False
+flaNose=False
+flaMouth=False
+
+xt=10
+yt=10
+
 
 while True:
     ret, frame = cap.read()
     frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    #for (x,y,w,h) in faces:
-        #img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        #roi_gray = gray[y:y+h, x:x+w]
-        #roi_color = img[y:y+h, x:x+w]
-            
-    mouth_rects = mouth_cascade.detectMultiScale(gray, 1.7, 11)
-    for (xm,ym,wm,hm) in mouth_rects:
-        ym = int(ym - 0.15*hm)
-        cv2.rectangle(frame, (xm,ym), (xm+wm,ym+hm), (0,255,0), 3)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        flagFace=True
+        xt=x
+        yt=y
         break
     
-    nose_rects = nose_cascade.detectMultiScale(gray, 1.3, 5)
-    for (xn,yn,wn,hn) in nose_rects:
-        cv2.rectangle(frame, (xn,yn), (xn+wn,yn+hn), (0,255,0), 3)
-        break
+    if flagFace:        
+        mouth_rects = mouth_cascade.detectMultiScale(gray, 1.7, 11)
+        for (xm,ym,wm,hm) in mouth_rects:
+            ym = int(ym - 0.15*hm)
+            cv2.rectangle(frame, (xm,ym), (xm+wm,ym+hm),(0,255,0), 3)
+            flaMouth=True
+            break
+            
+        
+        nose_rects = nose_cascade.detectMultiScale(gray, 1.3, 5)
+        for (xn,yn,wn,hn) in nose_rects:
+            cv2.rectangle(frame, (xn,yn), (xn+wn,yn+hn), (0,255,0), 3)
+            flaNose=True
+            break
+        #If any of the two parts is detected, then it will be a covidiot
+        if (flaMouth or flaNose):
+            cv2.putText(frame,"COVIDIOT DETECTED", (xt,yt), cv2.FONT_ITALIC, 0.5, (0,0,255))
+            
+        else:
+            cv2.putText(frame,"GOOD CITIZEN", (xt,yt), cv2.FONT_ITALIC, 0.5, (0,255,0))
+            
+        
+        
+    flag=False
+    flaNose=False
+    flaMouth=False
 
-    cv2.imshow('Mouth & Nose Detector', frame)
+    cv2.imshow('Mouth, Face & Nose Detector', frame)
 
     c = cv2.waitKey(1)
     if c == 27:
