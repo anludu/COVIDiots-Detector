f3cb519c691c824d6ca1dfe68c672e18dd625fae
David-Hernan <71787287+David-Hernan@users.noreply.github.com>
2020-12-03 13:51:25 -0600

    Reconocimiento de boca y nariz

---
+++ PFv1.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+nose_cascade = cv2.CascadeClassifier('haarcascade_mcs_nose.xml')
+mouth_cascade = cv2.CascadeClassifier('haarcascade_mcs_mouth.xml')
+
+
+if mouth_cascade.empty():
+  raise IOError('Unable to load the mouth cascade classifier xml file')
+  
+if nose_cascade.empty():
+  raise IOError('Unable to load the nose cascade classifier xml file')
+  
+cap = cv2.VideoCapture(0)
+ds_factor = 0.5
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    mouth_rects = mouth_cascade.detectMultiScale(gray, 1.7, 11)
+    for (x,y,w,h) in mouth_rects:
+        y = int(y - 0.15*h)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+        break
+    
+    nose_rects = nose_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in nose_rects:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+        break
+
+    cv2.imshow('Mouth & Nose Detector', frame)
+
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
